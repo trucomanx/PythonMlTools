d4fa0d921ac4432033627178481c51406807271a
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2023-05-23 03:26:07 -0300

    ordenando

--- src/PythonMlTools/Regression/RandomForestRegressorKfold.py
+++ src/PythonMlTools/Regression/RandomForestRegressorKfold.py
@@ -6,8 +6,8 @@ from sklearn.metrics import mean_absolute_percentage_error
 from tqdm.notebook import tqdm
 import numpy as np
 
-def FuncRandomForestRegressorKfold( param1_list,
-                                    param2_list,
+def FuncRandomForestRegressorKfold( n_estimators_list,
+                                    max_depth_list,
                                     X_train, 
                                     y_train,
                                     K=3,
@@ -15,15 +15,15 @@ def FuncRandomForestRegressorKfold( param1_list,
                                     min_samples_split=2,
                                     min_samples_leaf=2):
     found=False; k=0; 
-    pbar=tqdm(range(np.size(param1_list)));
-    SCORE_P1P2=np.zeros((np.size(param1_list),np.size(param2_list)))
+    pbar=tqdm(range(np.size(n_estimators_list)));
+    SCORE_P1P2=np.zeros((np.size(n_estimators_list),np.size(max_depth_list)))
     for j in pbar:
-        param1=param1_list[j]
+        n_estimators=n_estimators_list[j]
         score_val=[];
         ng=0;
-        for param2 in param2_list:
-            rfr = RandomForestRegressor(    n_estimators=param1,
-                                            max_depth=param2,
+        for max_depth in max_depth_list:
+            rfr = RandomForestRegressor(    n_estimators=n_estimators,
+                                            max_depth=max_depth,
                                             random_state=random_state,
                                             min_samples_split=min_samples_split,
                                             min_samples_leaf=min_samples_leaf);
@@ -39,26 +39,26 @@ def FuncRandomForestRegressorKfold( param1_list,
             
             score_val.append(sv);
             if k==0:
-                param1_opt=param1;
-                param2_opt=param2;
+                n_estimators_opt=n_estimators;
+                max_depth_opt=max_depth;
                 score_opt=sv;
                 rfr_opt=rfr;
                 
                 found=True;
             else:
                 if sv>score_opt:
-                    param1_opt=param1;
-                    param2_opt=param2;
+                    n_estimators_opt=n_estimators;
+                    max_depth_opt=max_depth;
                     score_opt=sv;
                     rfr_opt=rfr;
                     
                     cad="";
                     cad=cad+"R^2 val: %.3f" % sv;
                     cad=cad+" (%.3f)" % sv_std;
-                    cad=cad+"\tn_estimators:%d" % param1;
-                    cad=cad+"\tmax_depth:%d" % param2;
+                    cad=cad+"\tn_estimators:%d" % n_estimators;
+                    cad=cad+"\tmax_depth:%d" % max_depth;
                     pbar.set_description(cad);
-                    #pbar.set_description("R^2 val:"+str(sv)+" ("+str(sv_std)+")\tparam1:"+str(param1)+"\tparam2:"+str(param2));
+                    #pbar.set_description("R^2 val:"+str(sv)+" ("+str(sv_std)+")\tn_estimators:"+str(n_estimators)+"\tmax_depth:"+str(max_depth));
                     found=True;
             k=k+1
             ng=ng+1;
@@ -71,25 +71,25 @@ def FuncRandomForestRegressorKfold( param1_list,
     
     #print("rfr_opt:\n",rfr_opt.get_params(),"\n")
     
-    return rfr_opt, param1_opt, param2_opt, score_val_opt, SCORE_P1P2
+    return rfr_opt, n_estimators_opt, max_depth_opt, score_val_opt, SCORE_P1P2
 
 import matplotlib.pyplot as plot
 
 def FuncPlotDataKfold(  rfr_opt, 
-                        param1_opt, 
-                        param2_opt, 
+                        n_estimators_opt, 
+                        max_depth_opt, 
                         score_val_opt,
                         mean_y,std_y,
-                        param2_list,
+                        max_depth_list,
                         X_test, y_test):
     plot.figure(figsize=(6, 5))
-    plot.plot(param2_list, score_val_opt);
-    plot.xlabel('param2.');
+    plot.plot(max_depth_list, score_val_opt);
+    plot.xlabel('max_depth.');
     plot.ylabel('R^2 val');
-    plot.title("param1:"+str(param1_opt))
+    plot.title("n_estimators:"+str(n_estimators_opt))
 
     print(" ")
-    print("param1_opt: ",param1_opt,"\tparam2_opt:",param2_opt);
+    print("n_estimators_opt: ",n_estimators_opt,"\tmax_depth_opt:",max_depth_opt);
     print(" ")
     
     R2_val=np.max(score_val_opt);
@@ -119,33 +119,33 @@ def FuncPlotDataKfold(  rfr_opt,
     
     return R2_val, R2_test, MAPE;
     
-def FuncContourFRfrDataKfold(  param1_list,
-                            param2_list,
+def FuncContourFRfrDataKfold(  n_estimators_list,
+                            max_depth_list,
                             SCORE_P1P2,
                             nlevels=32,
                             title='',
                             cmap_str='jet'):
     # plot
     fig, ax = plot.subplots(figsize=(12, 12));
-    XXX, YYY = np.meshgrid(param1_list, param2_list)
+    XXX, YYY = np.meshgrid(n_estimators_list, max_depth_list)
     #print('SCORE_P1P2.shape:',SCORE_P1P2.shape);
     #print('     XXX.shape:',XXX.shape);
     #print('     YYY.shape:',YYY.shape);
     #im=ax.pcolormesh(XXX.T, YYY.T, SCORE_P1P2);
     levels = np.linspace(SCORE_P1P2.min(), SCORE_P1P2.max(), nlevels);
     im=ax.contourf(XXX.T, YYY.T, SCORE_P1P2,levels=levels,cmap=cmap_str);
-    ax.set_xlabel('param1');
-    ax.set_ylabel('param2');
+    ax.set_xlabel('n_estimators');
+    ax.set_ylabel('max_depth');
     ax.set_title(title);
     plot.colorbar(im,label="R2", orientation="vertical") ;
     plot.show();
 
-def FuncSurfaceRfrDataKfold(param1_list,
-                            param2_list,
+def FuncSurfaceRfrDataKfold(n_estimators_list,
+                            max_depth_list,
                             SCORE_P1P2,
                             title='',
                             cmap_str='jet'):
-    XXX, YYY = np.meshgrid(param1_list, param2_list);
+    XXX, YYY = np.meshgrid(n_estimators_list, max_depth_list);
     # plot
     fig = plot.figure(figsize=(12, 12));
     ax = plot.axes(projection='3d');
@@ -155,8 +155,8 @@ def FuncSurfaceRfrDataKfold(param1_list,
                     cstride=1, 
                     cmap=cmap_str#, edgecolor='none'
                    );
-    ax.set_xlabel('param1');
-    ax.set_ylabel('param2');
+    ax.set_xlabel('n_estimators');
+    ax.set_ylabel('max_depth');
     ax.set_zlabel('R2');
     ax.set_title(title);
     #ax.view_init(60, 35);
