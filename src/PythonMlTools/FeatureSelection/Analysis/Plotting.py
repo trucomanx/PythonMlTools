8f4d44d1c24a6bd1a9b461ce402a2eca4c5b5ca1
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2023-07-07 16:48:42 -0300

    ordenando

--- src/PythonMlTools/FeatureSelection/Analysis/Plotting.py
+++ src/PythonMlTools/FeatureSelection/Analysis/Plotting.py
@@ -14,11 +14,11 @@ def plot_bar_vec_xy(  VEC_XY,
                       horizontal=True):
     
     if horizontal:
+        fig = plt.figure(figsize=(figxsize, figysize)) # width and height in inches
+        plt.bar(labels_x, VEC_XY, edgecolor="white", linewidth=0.7,tick_label=labels_x)
+    else:
         fig = plt.figure(figsize=(figysize, figxsize)) # width and height in inches
         plt.barh(labels_x, VEC_XY, edgecolor="white", linewidth=0.7)
-    else:
-        fig = plt.figure(figsize=(figxsize, figysize)) # width and height in inches
-        plt.bar(labels_x, VEC_XY, edgecolor="white", linewidth=0.7)
     if isinstance(label_y,str):
         plt.ylabel(label_y);
     plt.title(title);
